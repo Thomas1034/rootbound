0c44f7239d9c3232d69ba3b1c31de69e2900ef38
Thomas Kern <142808316+Thomas1034@users.noreply.github.com>
2026-02-28 01:17:10 -0500

    Changed which bus the reload event is registered to. Of _course_ it's picky.

--- neoforge/src/main/java/com/startraveler/rootbound/Rootbound.java
+++ neoforge/src/main/java/com/startraveler/rootbound/Rootbound.java
@@ -15,6 +15,7 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
 import net.neoforged.neoforge.event.BlockEntityTypeAddBlocksEvent;
 import net.neoforged.neoforge.registries.DataPackRegistryEvent;
@@ -40,6 +41,14 @@ public class Rootbound {
         CommonClass.init();
 
         modBus.addListener(this::registerDatapackRegistries);
+
+        NeoForge.EVENT_BUS.addListener(
+                AddServerReloadListenersEvent.class,
+                event -> event.addListener(
+                        Identifier.fromNamespaceAndPath(Constants.MOD_ID, "clear_cache"),
+                        BlockTransformer.SAFE_CACHE
+                )
+        );
     }
 
     @SuppressWarnings("unused")
@@ -89,12 +98,6 @@ public class Rootbound {
                 })
         );
 
-        modBus.addListener(AddServerReloadListenersEvent.class,
-                event -> event.addListener(
-                        Identifier.fromNamespaceAndPath(Constants.MOD_ID, "clear_cache"),
-                        BlockTransformer.SAFE_CACHE
-                )
-        );
     }
 
     public void registerDatapackRegistries(final DataPackRegistryEvent.NewRegistry event) {
